d69a3d14269ff851f50c11bb6a8dde8cee51303d
quynhmanh.leo <quynhmanh.leo@gmail.com>
2015-04-12 22:38:45 +0700

    gg

---
+++ src/htmlCreator/Person.java
@@ -0,0 +1,14 @@
+package htmlCreator;
+
+import java.util.ArrayList;
+
+public class Person {
+	String id;
+	String name;
+	String field;
+	int year;
+	String birth;
+	String[] friends;
+	String decription;
+	String[] favorites;
+}
